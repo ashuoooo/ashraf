1ed0f066e505dd0852fb1081f7e4c739b0cc2831
ashuoooo <shaiekashu@gmail.com>
2025-03-20 23:01:42 +0530

    oka pratise match aduthuna

---
+++ development.java
@@ -0,0 +1,17 @@
+public class development{
+    public static void main(String[] args) {
+        // Printing a welcome message
+        System.out.println("Hello, Welcome to Java Programming!");
+        
+        int a = 10;
+        int b = 5;
+        
+        int sum = a + b;
+        int product = a * b;
+        
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+        System.out.println("Product of " + a + " and " + b + " is: " + product);
+    }
+
+
+}
